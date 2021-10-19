3d49a0195866503c535584bf44f8cfb3bb62617b
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-10-19 07:40:47 +0530

    Refactor

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,27 +2,27 @@
 
 StudentClass sclass = new(1, "Jon Doe");
 ChangeStudentClass(sclass);
-WriteLine($"{sclass.Id} {sclass.Name}"); // output: 2 Bob
+WriteLine($"ChangeStudentClass: {sclass.Id} {sclass.Name}");
 
 StudentClass sclass1 = new(1, "Jon Doe");
 ChangeStudentClass1(sclass1);
-WriteLine($"{sclass1.Id} {sclass1.Name}"); // output: 1 Jon Doe
+WriteLine($"ChangeStudentClass1: {sclass1.Id} {sclass1.Name}");
 
 StudentStruct sstruct = new(1, "Jon Doe");
 ChangeStudentStruct(sstruct);
-WriteLine($"{sstruct.Id} {sstruct.Name}"); // output: 1 Jon Doe
+WriteLine($"ChangeStudentStruct: {sstruct.Id} {sstruct.Name}");
 
 StudentStruct sstruct1 = new(1, "Jon Doe");
 ChangeStudentStruct1(ref sstruct1);
-WriteLine($"{sstruct1.Id} {sstruct1.Name}"); // output: 2 Bob
+WriteLine($"ChangeStudentStruct1: {sstruct1.Id} {sstruct1.Name}");
 
 StudentRecord srecord = new(1, "Jon Doe");
 ChangeStudentRecord(srecord);
-WriteLine($"{srecord.Id} {srecord.Name}"); // output: 2 Bob
+WriteLine($"ChangeStudentRecord: {srecord.Id} {srecord.Name}");
 
 StudentRecord srecord1 = new(1, "Jon Doe");
 ChangeStudentRecord1(srecord1);
-WriteLine($"{srecord1.Id} {srecord1.Name}"); // output: 1 Jon Doe
+WriteLine($"ChangeStudentRecord1: {srecord1.Id} {srecord1.Name}");
 
 static void ChangeStudentClass(StudentClass s)
 {
@@ -96,3 +96,14 @@ record StudentRecord
         Name = name;
     }
 }
+
+/*
+Output:
+
+ChangeStudentClass: 2 Bob
+ChangeStudentClass1: 1 Jon Doe
+ChangeStudentStruct: 1 Jon Doe
+ChangeStudentStruct1: 2 Bob
+ChangeStudentRecord: 2 Bob
+ChangeStudentRecord1: 1 Jon Doe
+*/
